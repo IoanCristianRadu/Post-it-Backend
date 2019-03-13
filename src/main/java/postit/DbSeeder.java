6814fea03ea072d7810c0e2fe0f18fa583360ffa
IoanCristianRadu <raduioancristian@gmail.com>
2019-03-13 20:06:35 +0100

    Database initialisation data improved.

--- src/main/java/postit/DbSeeder.java
+++ src/main/java/postit/DbSeeder.java
@@ -4,8 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import postit.account.Account;
 import postit.account.AccountRepository;
+import postit.comment.Comment;
 import postit.post.Post;
 import postit.post.PostRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class DbSeeder implements CommandLineRunner {
@@ -19,22 +23,75 @@ public class DbSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Post post1 = new Post("Title 1","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales, lorem quis scelerisque ultrices, augue velit gravida lectus, at fringilla erat mauris eget magna. Morbi ut odio sed ligula aliquam iaculis vehicula a dui. Suspendisse a quam odio. Donec metus neque, porttitor vel ultrices eget, cursus non leo. Aenean vestibulum, urna ac vehicula egestas, sem mi pellentesque lectus, convallis faucibus nisl odio ut arcu. Etiam eget lacus sed diam cursus volutpat pretium a nisl. Morbi nec viverra nunc. Suspendisse vulputate fermentum justo. Duis tincidunt venenatis dui. Praesent ultricies ex est, in euismod turpis sodales a. Aenean vitae sem id dolor iaculis venenatis. Sed non metus volutpat, cursus mauris in, ornare nisi. Sed sed mattis enim.",
-                "admin", "");
-        Post post2 = new Post("Title 2", "Morbi eu odio dui. Sed varius lectus in risus vestibulum cursus eu in libero. Etiam sollicitudin mauris venenatis arcu sollicitudin sodales. Aenean non aliquam eros. Praesent elementum lorem dolor, et viverra magna pretium eget. Ut varius lacus ut malesuada maximus. Pellentesque nec metus a mi viverra semper. Fusce luctus posuere lacus, in porttitor tellus. Maecenas nunc magna, fermentum quis tortor eget, feugiat ornare quam. Phasellus faucibus, dui vel porttitor feugiat, lectus dui sodales purus, at molestie eros nibh non eros. Curabitur ut placerat velit. Nunc at sagittis eros. Aenean pharetra risus id eros interdum, sed vulputate massa sagittis.",
-                "admin", "https://previews.123rf.com/images/larryrains/larryrains1605/larryrains160500369/57278803-bottle-of-water.jpg");
-        Post post3 = new Post("My favorite kaktus picture" , "content", "admin", "https://media.istockphoto.com/photos/cactus-isolated-on-white-picture-id636856414?k=6&m=636856414&s=612x612&w=0&h=mIx1wbRW5I6gc6u-r6tduiaE_iU85gh8mMM2BUJlB04=");
         //drop all posts
         this.postRepository.deleteAll();
+        ArrayList<Comment> arrayListComment = new ArrayList<>();
+
+        arrayListComment.add(new Comment("mary",
+                "The grid system is really cool, each area get it's own little plaza.", 0));
+
+        arrayListComment.add(new Comment("andrew",
+                "That was the original idea, but not the final execution. Zoom in and check. All the \"inner plazas\" in the picture are the roofs of warehouses, parkings, supermarkets... at ground level.\n" +
+                        "\n" +
+                        "The only 2 parks in this picture are next to Sagrada Familia and are open parks, not inner.\n" +
+                        "\n" +
+                        "PS: There are quite a few inner parks scattered through the city, though.", 0));
+
+        arrayListComment.add(new Comment("marcus",
+                "Wow the difference is like night and day", 0));
+        arrayListComment.add(new Comment("oscar",
+                "As a northern European I gotta say this image of perfection makes me shudder. The city without curves and bends and lakes in-between and more organic structures isn't something I'd wanna live in... Impressive shot tho", 0));
+
+        Post post1 = new Post("Barcelona, day and night", "The Urban planner was a revolutionist well ahead of his time, he'd incorporated roads of larger width and corners to accommodate the usage of horse carriages for example which has now allowed easier traffic flow and regular spaces for taxi pick up and drop off. Understanding the importance of maintaing the dichotomy of urbanism and nature, he also incorporated open spaces, parks and greenery within most blocks as well. He provides a fantastic example of thinking ahead of your current time and means, and really incorporating future scenarios. Now his work maintains practicality and beauty all these years, Ildefons Cerdà's philosophy and legacy lives on.",
+                "mary", "https://external-preview.redd.it/wtyyftyAgZ_8TL-c9Aej2EEbwwzsRToso0jTNW6y11M.jpg?auto=webp&s=931afd93ff17aea2aa82bf6bed8cca5797987b19");
+        post1.setComments(arrayListComment);
 
         //add posts to database
         this.postRepository.save(post1);
+
+
+        arrayListComment.clear();
+        arrayListComment.add(new Comment("mary",
+                "If the test actually misspelled \"finish\" it would be technically correct ... but this is just throwing away points for trying to be funny. Not worth it.", 0));
+
+        arrayListComment.add(new Comment("andrew",
+                "This joke was in a Danish line of commercials that ran from 1989. Its a shop keeper and a customer who cant pronounce the name of the product - in this one he took a English lesson on tape to finally make it.\n" +
+                        "\n" +
+                        "https://www.youtube.com/watch?v=knsaerPhmgw\n" +
+                        "\n" +
+                        "I can imagine the Finnish people being sick and tired of this joke, but it sure has shown its longevity.", 0));
+
+        Post post2 = new Post("Close!", "I like that agressive \"NO !!\" on the side too, it accurately represents the Finnish reaction to this.",
+                "andrew", "https://i.redd.it/j54dqiwioeu01.jpg");
+        post2.setComments(arrayListComment);
+
         this.postRepository.save(post2);
+
+
+        arrayListComment.clear();
+        arrayListComment.add(new Comment("marcus",
+                "I'm not exactly sure how I should rate this? Germany has almost the same size, but austria is smaller, even though the GDP per capita is a little bit higher.\n" +
+                        "What are the values? Is it based on GDP per km²?", 0));
+
+        arrayListComment.add(new Comment("andrew",
+                "This map is scaled based on GDP ber sqkm of a country.\n" +
+                        "It assumes that each km2 in scaled countries has GDP of 10 million usd.", 0));
+
+
+        Post post3 = new Post("Map of European countries sized according to their national GDP", "I did not expect France to be so small! Wow!",
+                "oscar", "https://i.redd.it/70to23y1iul21.png");
+        post3.setComments(arrayListComment);
         this.postRepository.save(post3);
 
-        Account account = new Account("admin","admin");
+        Account account1 = new Account("admin", "admin");
+        Account account2 = new Account("marcus", "marcus");
+        Account account3 = new Account("andrew", "andrew");
+        Account account4 = new Account("mary", "mary");
 
         this.accountRepository.deleteAll();
-        this.accountRepository.save(account);
+        this.accountRepository.save(account1);
+        this.accountRepository.save(account2);
+        this.accountRepository.save(account3);
+        this.accountRepository.save(account4);
     }
 }
